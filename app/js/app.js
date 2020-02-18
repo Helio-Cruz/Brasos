c49181ce0041bdea989c3760d5cc7c0a6555f7a6
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-02-18 08:55:10 +0100

    import uikit

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,10 @@
 import UIkit from 'uikit';
- import Icons from 'uikit/dist/js/uikit-icons';
+import Icons from 'uikit/dist/js/uikit-icons';
 var app = {
   init: function() {
     console.log('init');
     // loads the Icon plugin
-         UIkit.use(Icons);
-    // components can be called from the imported UIkit reference
-       UIkit.notification('Hello world.');
+    UIkit.use(Icons);
   }
 };
 
