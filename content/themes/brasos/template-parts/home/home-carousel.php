6dd6297140ce835d6589ab1f96ab294accc32f67
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-03 19:07:34 +0100

    carousel

--- content/themes/brasos/template-parts/home/home-carousel.php
+++ content/themes/brasos/template-parts/home/home-carousel.php
@@ -1,6 +1,6 @@
 
   <!-- CAROUSEL -->
-  <div class="carousel-images uk-position-relative uk-light" tabindex="-1" uk-slideshow>
+  <div class="carousel-images uk-position-relative uk-light" tabindex="-1" uk-slideshow="autoplay:true; autoplay-interval:4000; min-height:300; max-height:600">
     <ul class="uk-slideshow-items">
       <li>
         <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-center-left">
@@ -17,7 +17,7 @@
           <img src="<?= get_theme_mod('brasos_bg_slide_2'); ?>" alt="">
         </div>
         <div class="uk-position-bottom-right uk-position-medium uk-light">
-          <h1 class="h1-title">BRASOS<br>
+          <h1 class="h1-title"><span>BRASOS</span><br>
             Brazilian Society for Osteoarthritis, Osteoporosis and Sarcopenia -<br>
             Sociedade de médicos de diferentes especialidades</h1>
         </div>
@@ -27,7 +27,7 @@
           <img src="<?= get_theme_mod('brasos_bg_slide_3'); ?>" alt="">
         </div>
         <div class="uk-position-bottom-right uk-position-medium uk-light">
-          <h1 class="h1-title">BRASOS<br>
+          <h1 class="h1-title"><span>BRASOS</span><br>
             Brazilian Society for Osteoarthritis, Osteoporosis and Sarcopenia -<br>
             Sociedade de médicos de diferentes especialidades</h1>
         </div>
