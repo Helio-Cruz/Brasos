a9187bc5b4e73e9c0ffc9724a0c24d44334ea315
helio <helio-cruz@hotmail.com>
2020-03-04 17:31:07 +0100

    adding wp congress

--- content/themes/brasos/template-congress.php
+++ content/themes/brasos/template-congress.php
@@ -6,35 +6,97 @@ Template Name: Congressos
 
 <?php get_header(); ?>
 
-<div class="articles-cards">
-    <h2 class="h2-title"><?php the_title(); ?></h2>
-    <div class="uk-child-width-1-1" uk-grid>
-      <div>
-        <div class="uk-card uk-card-default" style="background-image: url('<?php echo get_template_directory_uri() . '/public/images/articles.jpg ' ?>');">
-          <a href="blog.html">
-            <div class="uk-card-body">
-              <h3 class="uk-card-title h3-title">Titulo da noticia mais recente</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-            </div>
-          </a>
+
+<div class="congress__content">
+    <h2 class="h2-title">Congressos</h2>
+    <div uk-flex uk-flex-center uk-grid>
+      <div class="uk-card uk-card-default">
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="https://www.isiat2019.com/" target="_blank">ISIAT 2019</a></h3>
+        </div>
+        <div class="uk-card-body">
+          <p>Local: Lisboa - Portugal
+            <br><br>
+            Data: 03 a 05 de outubro de 2019
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="http://www.medicinadoesporte.org.br/" target="_blank">31º CONGRESSO BRASILEIRO DE
+              MEDICINA
+              DO
+              EXERCÍCIO E DO ESPORTE E O XI CONGRESSO SUL-AMERICANO DE MEDICINA DO ESPORTE</a></h3>
+        </div>
+        <div class="uk-card-body">
+          <p>Local: Bourbon Convention - Foz do Iguaçu, PR - Brasil
+            <br><br>
+            Data: 29 a 31 de agosto de 2019
+            <br><br>
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="https://www.reumatologia.org.br/" target="_blank">XXXVI CONGRESSO BRASILEIRO DE
+              REUMATOLOGIA</a></h3>
+        </div>
+
+        <div class="uk-card-body">
+          <p>Local: Fortaleza, CE - Brasil
+            <br><br>
+            Data: 04 a 07 de setembro de 2019
+            <br><br>
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="www.eularcongress2019.org" target="_blank">EULAR ANNUAL CONGRESS 2019</a></h3> <!--link quebrado-->
+
+        </div>
+        <div class="uk-card-body">
+          <p>Local: Madrid - Espanha
+            <br><br>
+            Data: 12 a 15 de junho de 2019
+            <br><br>
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="www.portalsbot.org.br" target="_blank"><!--link quebrado-->
+            XXV CONGRESSO BRASILEIRO DE
+              TRAUMA
+              ORTOPÉDICO</a></h3>
+        </div>
+        <div class="uk-card-body">
+          <p>Local: Curitiba, PR - Brasil
+            <br><br>
+            Data: 09 a 11 de maio de 2019
+            <br><br>
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="https://oarsi.org/" target="_blank">OARSI 2019 WORLD CONGRESS</a></h3>
+        </div>
+        <div class="uk-card-body">
+          <p>Local: Toronto, ON - Canada
+            <br><br>
+            Data: 02 a 05 de maio de 2019
+            <br><br>
+          </p>
         </div>
       </div>
     </div>
-    <div class="uk-child-width-1-3@m" uk-grid>
-
-        
-    <?php
-        $args = ['category_name' => 'Congresso'];
-        $wp_query = new WP_Query($args);
-        if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
-    ?>
-
-                        <?php get_template_part('template-parts/post/article', 'excerpt'); ?>
-
-        
-       <?php endwhile;
-        endif;  ?>
-     
-    </div>
   </div>
+
+
 <?php get_footer(); ?>
