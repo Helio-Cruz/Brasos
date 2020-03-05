11dca1ddb45c28420cd7d8141ec704b66110def5
helio <helio-cruz@hotmail.com>
2020-03-05 22:34:17 +0100

    ...

--- content/themes/brasos/404.php
+++ content/themes/brasos/404.php
@@ -1,2 +1,7 @@
 <?php get_header(); ?>
+<div uk-scrollspy="cls: uk-animation-slide-bottom; target: .title; delay: 500; repeat: true">
+    <div class="not-found">
+    <h2 class="not-found__text title">Desculpe, não podemos encontrar o evento que você está procurando</h2>
+ </div>
+</div>
 <?php get_footer(); ?>
