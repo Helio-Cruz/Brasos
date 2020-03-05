22744e552a0ce3df721cbe978029077d4d8b2b08
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-05 22:18:37 +0100

    events + navbars

--- content/themes/brasos/template-parts/header/header-navbar.php
+++ content/themes/brasos/template-parts/header/header-navbar.php
@@ -12,13 +12,13 @@
        <ul class="uk-navbar-nav">
          <li><a href="<?php echo home_url(); ?>">Home</a></li>
          <li>
-           <a href="">Institucional</a>
+           <a>Institucional</a>
            <div class="uk-navbar-dropdown">
              <ul class="uk-nav uk-navbar-dropdown-nav">
                <?php
                 $menu = wp_nav_menu([
-                  'menu' => 'subnav',
-                  'theme_location' => 'menu-top',
+                  'menu' => 'subnav-institucional',
+                  'theme_location' => '',
                   'container' => '',
                   'items_wrap' => '<li><a href="%2$s">%3$s</a></li>',
                   'echo' => true
@@ -30,14 +30,13 @@
            </div>
          </li>
          <li>
-           <a href="">Eventos</a>
+           <a href="<?php echo home_url( '/eventos/' )?>">Eventos</a>
            <div class="uk-navbar-dropdown">
              <ul class="uk-nav-sub uk-navbar-dropdown-nav">
                <?php
                 $menu = wp_nav_menu([
-                  'menu' => 'menu-header',
-                  //  'theme_location' => 'menu-top',
-                  // 'container_class' => '',
+                  'menu' => 'subnav-eventos',
+                  'theme_location' => '',
                   'container' => '',
                   'items_wrap' => '<li><a href="%2$s">%3$s</a></li>',
                   'echo' => true
@@ -51,8 +50,7 @@
          <?php
           $menu = wp_nav_menu([
             'menu' => 'menu-header',
-            //  'theme_location' => 'menu-top',
-            // 'container_class' => '',
+            'theme_location' => '',
             'container' => '',
             'items_wrap' => '<li><a href="%2$s">%3$s</a></li>',
             'echo' => true
@@ -66,7 +64,6 @@
            <?php
             $menu = wp_nav_menu([
               'menu' => 'flag',
-              'container_class' => '',
               'container' => '',
               'items_wrap' => '<a href="%2$s">%3$s</a>',
               'echo' => true
@@ -79,52 +76,4 @@
        <a href="#" class="uk-navbar-toggle" uk-navbar-toggle-icon uk-toggle="target: #sidenav"></a>
      </div>
    </nav>
- </div>
- <div id="sidenav" uk-offcanvas="overlay: true; flip: true">
-   <div class="uk-offcanvas-bar uk-background-default">
-     <button class="uk-offcanvas-close" type="button" uk-close></button>
-     <ul class="uk-nav">
-       <div class="flag">
-         <li>
-           <a href="#">
-             <img src="<?php echo get_template_directory_uri() . '/public/images/flag/brazil.png' ?>" alt="">
-           </a>
-         </li>
-         <li>
-           <a href="#">
-             <img src="<?php echo get_template_directory_uri() . '/public/images/flag/usa.png' ?>" alt="">
-           </a>
-         </li>
-       </div>
-       <li class="uk-nav-divider"></li>
-       <li><a href="index.html">Home</a></li>
-       <li class="uk-parent">
-         <a href="">Institucional</a>
-         <ul class="uk-nav-sub uk-navbar-dropdown-nav">
-           <li><a href="institutional.html">Brasos</a></li>
-           <li><a href="law.html">Estatuto Abeti</a></li>
-           <li><a href="words.html">Palavras do Presidente</a></li>
-           <li><a href="directory.html">Conselho Diretor</a></li>
-         </ul>
-       </li>
-       <li class="uk-parent">
-         <a href="">Eventos</a>
-         <ul class="uk-nav-sub uk-navbar-dropdown-nav">
-           <li><a href="">BRASIT 2020</a></li>
-           <li><a href="">BRASOStbt 2019</a></li>
-           <li><a href="">BRASIT 2018</a></li>
-         </ul>
-       </li>
-       <li><a href="articles.html">Artigos</a></li>
-       <li><a href="contact.html">Contato</a></li>
-       <li><a href="member.html">Torne-se membro</a></li>
-     </ul>
-   </div>
- </div>
- <!-- MENU MOBILE -->
- <div class="menu-mobile" uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky;">
-   <div class="header-nav__logo">
-     <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/public/images/logo.png' ?>" alt=""></a>
-   </div>
-   <a href="#" class="uk-navbar-toggle" uk-navbar-toggle-icon uk-toggle="target: #sidenav"></a>
  </div>
---
+++ content/themes/brasos/template-parts/post/event-excerpt.php
@@ -0,0 +1,7 @@
+<div>
+  <div class="uk-card uk-card-default">
+    <a href="<?php the_permalink(); ?>">
+      <img class="articles-cards-image-events" src="<?php the_post_thumbnail_url(); ?>" alt="">
+    </a>
+  </div>
+</div>
