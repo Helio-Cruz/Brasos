706291d4a360a8bd9f177bff8eba4d7388844340
helio <helio-cruz@hotmail.com>
2020-03-05 21:24:44 +0100

    adding animations

--- content/themes/brasos/header.php
+++ content/themes/brasos/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="#">
+  <link rel="icon" href=" <?php echo get_template_directory_uri() . '/public/images/favicon.ico' ?>">
   <title><?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="scss/main.scss" type="text/scss" />
