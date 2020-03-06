4b3091f7d2f6c38ee2f2c77beeb8fef9a4226ea7
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-06 11:03:24 +0100

    form

--- content/themes/brasos/template-events.php
+++ content/themes/brasos/template-events.php
@@ -6,30 +6,26 @@ Template Name: Eventos
 <?php get_header(); ?>
 
 <div class="articles-cards">
-  <h2 class="h2-title"><?php the_title(); ?></h2>
-  <div class="uk-child-width-1-3@m" uk-grid>
-    <?php
-    $args = ['category_name' => 'Evento'];
-    $wp_query = new WP_Query($args);
-    if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
-    ?>
-
-        <?php get_template_part('template-parts/post/event', 'excerpt'); ?>
-
-
-    <?php endwhile;
-    endif;  ?>
-
-  </div>
-</div>
-
-<div class="articles-cards">
   <h2 class="h2-title">Eventos</h2>
   <div class="uk-child-width-1-3@m" uk-grid>
     <div>
       <div class="uk-card uk-card-default">
+        <a href="<?php echo home_url( '/brasit2020-informations/' )?>">
+          <img class="articles-cards-image-events" src="<?php echo get_template_directory_uri() . '/public/images/logo-brasit2020.jpg' ?>" alt="">
+        </a>
+      </div>
+    </div>
+    <div>
+      <div class="uk-card uk-card-default">
         <a href="<?php echo home_url( '/brasos2019-informations/' )?>">
           <img class="articles-cards-image-events" src="<?php echo get_template_directory_uri() . '/public/images/logo-brasostbt.jpg' ?>" alt="">
+        </a>
+      </div>
+    </div>
+    <div>
+      <div class="uk-card uk-card-default">
+        <a href="<?php echo home_url( '/brasit2018-informations/' )?>">
+          <img class="articles-cards-image-events" src="<?php echo get_template_directory_uri() . '/public/images/logo-brasit2018.jpg' ?>" alt="">
         </a>
       </div>
     </div>
