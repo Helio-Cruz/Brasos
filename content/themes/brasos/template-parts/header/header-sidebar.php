a8a3441d64f9f17cb63595392500c27af5291175
helio <helio-cruz@hotmail.com>
2020-03-06 22:40:22 +0100

    ...

--- content/themes/brasos/template-parts/header/header-sidebar.php
+++ content/themes/brasos/template-parts/header/header-sidebar.php
@@ -5,14 +5,14 @@
     <div class="white-block">
       <ul class="uk-nav">
         <div class="flag">
-          <?php
+        <?php
           $menu = wp_nav_menu([
             'menu' => 'flag',
-            'container' => '',
-            'items_wrap' => '<a href="%2$s">%3$s</a>',
+            'container' => false,
+            'items_wrap' => '%3$s',
             'echo' => true
           ]);
-          $menu = strip_tags($menu, '<li></li>');
+          $menu = strip_tags($menu, '<li><a href="%2$s">');
           echo $menu;
           ?>
         </div>
@@ -49,29 +49,29 @@
             $menu = strip_tags($menu, '');
             echo $menu;
             ?> -->
-        <?php
+            <?php
         $menu = wp_nav_menu([
           'menu' => 'menu-header',
-          'container' => '',
-          'items_wrap' => '<li><a href="%2$s">%3$s</a></li>',
+          'container' => false,
+          'items_wrap' => '%3$s',
           'echo' => true
         ]);
-        $menu = strip_tags($menu, '');
+        $menu = strip_tags($menu, '<li><a href="%2$s">');
         echo $menu;
         ?>
       </ul>
     </div>
     <div class="green-block">
       <div class="green-diagonal"></div>
-      <ul class="uk-nav mobile-nav-bottom">
+      <ul class="uk-nav uk-flex uk-flex-center uk-flex-wrap">
         <?php
         $menu = wp_nav_menu([
           'menu' => 'menu-footer',
-          'container' => '',
-          'items_wrap' => '<li><a href="%2$s">%3$s</a></li>',
+          'container' => false,
+          'items_wrap' => '%3$s',
           'echo' => true
         ]);
-        $menu = strip_tags($menu, '');
+        $menu = strip_tags($menu, '<li><a href="%2$s">');
         echo $menu;
         ?>
       </ul>
