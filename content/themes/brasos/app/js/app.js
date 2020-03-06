41aab9d796b471e9161770dbdf7d6f16cad4537e
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-07 00:10:24 +0100

    js

--- content/themes/brasos/app/js/app.js
+++ content/themes/brasos/app/js/app.js
@@ -1,6 +1,7 @@
 
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import { gsap } from "gsap";
 
 var app = {
   init: function () {
@@ -28,9 +29,43 @@ var app = {
     // }
     // );
 
+    
+      // window.onscroll = function () { scrollFunction() };
+      // gsap.timeline()
+      var ctlrManyElementsManyTimes = new ScrollMagic.Controller();
+      $("body > div").each(function () {
+        var fadeTop = $(this).find(".fadeTop");
+        var fadeLeft = $(this).find(".fadeTop");
+        var fadeRight = $(this).find(".fadeRight");
+        var fadeBottom = $(this).find(".fadeBottom");
+        var tl = new TimelineMax();
+        tl.fromTo(fadeTop, 0.8,
+          { opacity: 0, y:-50, x:0, ease: 'ease-out' },
+          { opacity: 1, y:0 }
+        )
+          .fromTo(fadeLeft, 0.8,
+            { opacity: 0, x:-50, y:0, ease: 'ease-out' },
+            { opacity: 1, x:0 }
+          )
+          .fromTo(fadeRight, 0.8,
+            { opacity: 0, x:50, y:0, ease: 'ease-out' },
+            { opacity: 1, x:0 }
+          )
+          .fromTo(fadeBottom, 0.8,
+            { opacity: 0, y:50, x:0, ease: 'ease-out' },
+            { opacity: 1, y:0 }
+          );
+        new ScrollMagic.Scene({
+          triggerElement: this,
+          triggerHook: "onCenter", // or onEnter
+          reverse: false
+        })
+          .setTween(tl)
+          .addTo(ctlrManyElementsManyTimes);
+      });
+
+
   }
-
-
 };
 
 $(app.init);
--- content/themes/brasos/header.php
+++ content/themes/brasos/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href=" <?php echo get_template_directory_uri() . '/public/images/favicon.ico' ?>">
   <title><?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="scss/main.scss" type="text/scss" />
+  <!-- <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="scss/main.scss" type="text/scss" /> -->
 
   <?php wp_head(); ?>
 </head>
