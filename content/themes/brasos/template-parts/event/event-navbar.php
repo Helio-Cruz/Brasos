4e6c2bcb724a2aa2681ef5818ada9eeabefce3f9
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-06 11:03:24 +0100

    form

--- content/themes/brasos/template-parts/event/event-navbar.php
+++ content/themes/brasos/template-parts/event/event-navbar.php
@@ -1,14 +1,14 @@
 <!-- SUBNAV -->
-<nav class="uk-navbar-container">
+<nav class="uk-navbar-container navbar-container-events">
   <div uk-navbar>
     <div class="uk-navbar-center">
-      <ul class="uk-navbar-nav navbar-events">
+      <ul class="uk-navbar-nav">
         <?php
         $menu = wp_nav_menu([
           'menu' => 'nav-brasos2019',
           'container_class' => '',
           'container' => '',
-          'items_wrap' => '<li><a href="%2$s">%3$s</a></li>',
+          'items_wrap' => '<li class="navbar-events"><a href="%2$s">%3$s</a></li>',
           'echo' => true
         ]);
         $menu = strip_tags($menu, '');
