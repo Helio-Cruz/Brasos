07dbd0da8656203220a3d61403b97070733bdaa4
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-08 15:23:22 +0100

    integration events

--- content/themes/brasos/single-evento.php
+++ content/themes/brasos/single-evento.php
@@ -3,20 +3,19 @@
 <!-- IMAGE COVER -->
 <div class="events__cover">
   <div class="uk-child-width-1-1" uk-grid>
-    <div class="uk-card uk-card-default" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
-      <?php the_title(); ?>
+    <div class="uk-card uk-card-default uk-text-middle" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+      <h1 class="h1-title uk-position-center"><?php the_title(); ?></h1>
     </div>
   </div>
 </div>
 
 <!-- NAVIGATION -->
-<!-- <ul class="uk-subnav uk-subnav-pill" uk-switcher>
+<ul class="uk-subnav navbar-events uk-height-small" uk-switcher>
   <li><a href="#">INFORMAÇÕES GERAIS</a></li>
-  <li><a href="#">CERTIFICADOS</a></li>
   <li><a href="#">PROGRAMAÇÃO</a></li>
   <li><a href="#">PALESTRANTES CONVIDADOS</a></li>
   <li><a href="#">INSCRIÇÕES/PAGAMENTO</a></li>
-</ul> -->
+</ul>
 
 <?php get_template_part('template-parts/post/event', 'full'); ?>
 
