f584d09aae6bff67db287a368a6acaf38a124058
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-08 07:21:02 +0100

    integration events beginning

--- content/themes/brasos/template-parts/post/event-excerpt.php
+++ content/themes/brasos/template-parts/post/event-excerpt.php
@@ -1,7 +1,8 @@
-<!-- <div>
-  <div class="uk-card uk-card-default">
-    <a href="<?php //the_permalink(); ?>">
-      <img class="articles-cards-image-events" src="<?php// the_post_thumbnail_url(); ?>" alt="">
+<div>
+  <div class="uk-card uk-card-default articles-cards-events">
+    <a href="<?php the_permalink(); ?>">
+      <!-- EVENTS : INFORMATIONS -->
+      <img class="articles-cards-image-events" src="<?php the_post_thumbnail_url(); ?>" alt="">
     </a>
   </div>
-</div> -->
+</div>
