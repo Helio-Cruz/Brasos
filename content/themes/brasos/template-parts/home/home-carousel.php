9df14a9a5cd9e7bafd9413e1920225b0a7cf0dcf
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-08 21:50:43 +0100

    slick carousel

--- content/themes/brasos/template-parts/home/home-carousel.php
+++ content/themes/brasos/template-parts/home/home-carousel.php
@@ -1,10 +1,9 @@
-
-  <!-- CAROUSEL -->
-  <div class="carousel-images uk-position-relative uk-light" tabindex="-1" uk-slideshow="autoplay:true; autoplay-interval:4000; min-height:300; max-height:600">
+<!-- CAROUSEL -->
+<!-- <div class="carousel-images uk-position-relative uk-light" tabindex="-1" uk-slideshow="autoplay:true; autoplay-interval:4000; min-height:300; max-height:600">
     <ul class="uk-slideshow-items">
       <li>
         <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-center-left">
-          <img src="<?= get_theme_mod('brasos_bg_slide_1'); ?>" alt="">
+          <img src="< ?= get_theme_mod('brasos_bg_slide_1'); ?>" alt="">
         </div>
         <div class="uk-position-bottom-right uk-position-medium uk-light">
           <h1 class="h1-title"><span>BRASOS</span><br>
@@ -14,7 +13,7 @@
       </li>
       <li>
         <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-center-left">
-          <img src="<?= get_theme_mod('brasos_bg_slide_2'); ?>" alt="">
+          <img src="< ?= get_theme_mod('brasos_bg_slide_2'); ?>" alt="">
         </div>
         <div class="uk-position-bottom-right uk-position-medium uk-light">
           <h1 class="h1-title"><span>BRASOS</span><br>
@@ -24,7 +23,7 @@
       </li>
       <li>
         <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-center-left">
-          <img src="<?= get_theme_mod('brasos_bg_slide_3'); ?>" alt="">
+          <img src="< ?= get_theme_mod('brasos_bg_slide_3'); ?>" alt="">
         </div>
         <div class="uk-position-bottom-right uk-position-medium uk-light">
           <h1 class="h1-title"><span>BRASOS</span><br>
@@ -37,4 +36,24 @@
       uk-slideshow-item="previous"></a>
     <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next
       uk-slideshow-item="next"></a>
+  </div> -->
+  
+<header>
+  <div class="slick-carousel">
+    <div>
+      <img src="<?php echo get_template_directory_uri() . '/public/images/d.1.jpg' ?>" alt="" />
+    </div>
+    <div>
+      <img src="<?php echo get_template_directory_uri() . '/public/images/d.2.jpg' ?>" alt="" />
+    </div>
+    <div>
+      <img src="<?php echo get_template_directory_uri() . '/public/images/d.3.jpg' ?>" alt="" />
+    </div>
   </div>
+  <div class="slick-carousel__text">
+    <h1 class="h1-title"><span>BRASOS</span><br>
+      Brazilian Society for Osteoarthritis, Osteoporosis and Sarcopenia -<br>
+      Sociedade de médicos de diferentes especialidades
+    </h1>
+  </div>
+</header>
