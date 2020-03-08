733d662bbbafae09c48c67fe274ba1cfc8b1dcfe
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-08 21:12:53 +0100

    integration events not ready

--- content/themes/brasos/template-events.php
+++ content/themes/brasos/template-events.php
@@ -11,22 +11,30 @@ Template Name: Eventos
   <div class="uk-child-width-1-3@m" uk-grid>
 
     <?php
-
+    $parent_cat = get_category_by_slug( $cat[1] );
     $args = [
       'post_type' => 'evento',
-      'category__in' => 1, // category 1 = parent "evento"
+      'parent'    => $parent_cat->term_id,
+      // 'category__in' => 1, // category 1 = parent "evento"
       'posts_per_page' => -1,
       'order' => 'DESC'
     ];
 
-    $wp_query = new WP_Query($args);
 
-    if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
+    $categories = get_categories( $args );
+    // loop on the child cats to get the sub cats object
+    foreach ($categories as $post) {
+     
+      
+
+    // $wp_query = new WP_Query($args);
+
+    // if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
         get_template_part('template-parts/post/event', 'excerpt');
-      endwhile;
-    endif;
+    //   endwhile;
+    // endif;
 
-    wp_reset_postdata();
+    // wp_reset_postdata();
     ?>
 
   </div>
