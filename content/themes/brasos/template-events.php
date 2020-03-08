f96f73689614af6a887e1451f44f8c15eae935cd
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-09 00:10:37 +0100

    slick carousel

--- content/themes/brasos/template-events.php
+++ content/themes/brasos/template-events.php
@@ -10,31 +10,24 @@ Template Name: Eventos
   <h2 class="h2-title"><?php the_title(); ?></h2>
   <div class="uk-child-width-1-3@m" uk-grid>
 
-  <?php
-      $args = [
-        'numberposts' => 1, // Number of recent posts thumbnails to display
-        'category_name'  => 'Artigo',
-      ];
-      foreach ($recent_posts as $post) :
-     
+    <?php
 
+    $args = [
+      'post_type' => 'evento',
+      'category__in' => 1, // category 1 = parent "evento"
+      'posts_per_page' => -1,
+      'order' => 'DESC'
+    ];
 
-    //  $categories = get_categories( $args );
-    // loop on the child cats to get the sub cats object
-    // foreach ($categories as $post) {
-     
-      
+    $wp_query = new WP_Query($args);
 
-    // $wp_query = new WP_Query($args);
-
-    // if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
+    if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
         get_template_part('template-parts/post/event', 'excerpt');
-    //   endwhile;
-    // endif;
+      endwhile;
+    endif;
 
-    // wp_reset_postdata();
-    endforeach;
-      wp_reset_query(); ?>
+    wp_reset_postdata();
+    ?>
 
   </div>
 </div>
