8018fd1fdf3f0dae7addf713beacceb0141358a8
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-09 03:37:06 +0100

    carousel

--- content/themes/brasos/template-parts/home/home-carousel-2.php
+++ content/themes/brasos/template-parts/home/home-carousel-2.php
@@ -1,105 +1,104 @@
 <!-- CAROUSEL -->
-<div class="carousel-small uk-light uk-position-relative" tabindex="-1"
-     uk-slideshow=" autoplay:true; autoplay-interval:4000; min-height:120; max-height:120">
-    <ul class="uk-slideshow-items">
-      <li>
+<div class="carousel-small uk-light uk-height-small uk-position-relative" tabindex="-1" uk-slideshow=" autoplay:true; autoplay-interval:4000; min-height:120; max-height:120">
+  <ul class="uk-slideshow-items">
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-1'); ?>
- 
-          </p>
-        </div>
-      </li>
-      <li>
+
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-2'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-3'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-4'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-5'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-6'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-7'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-8'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-9'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-10'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-11'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-12'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-13'); ?>
-          </p>
-        </div>
-      </li>
-      <li>
+        </p>
+      </div>
+    </li>
+    <li>
       <div class="uk-position-center uk-position-small uk-text-center uk-light">
-          <p class="uk-margin-remove">
+        <p class="uk-margin-remove">
           <?= get_theme_mod('brasos_call_to_content-14'); ?>
-          </p>
-        </div>
-      </li>
-    </ul>
-  </div>
+        </p>
+      </div>
+    </li>
+  </ul>
+</div>
