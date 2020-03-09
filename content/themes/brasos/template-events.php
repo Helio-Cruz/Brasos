0145db578c13c274756e787b2348a0fb0b1ff8cb
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-09 20:51:36 +0100

    nada ainda

--- content/themes/brasos/template-events.php
+++ content/themes/brasos/template-events.php
@@ -14,8 +14,7 @@ Template Name: Eventos
 
     $args = [
       'post_type' => 'evento',
-      'parent' => 0,
-      'category__in' => '31,19,21', // category 1 = parent "evento"
+      'category__in' => 1, // category 1 = parent "evento"
       'posts_per_page' => -1,
       'order' => 'DESC'
     ];
