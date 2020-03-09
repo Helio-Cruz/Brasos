41ba2fa18a0f6424a61138600f8b71cc9381d95d
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-09 03:37:06 +0100

    carousel

--- content/themes/brasos/template-parts/post/event-excerpt.php
+++ content/themes/brasos/template-parts/post/event-excerpt.php
@@ -1,7 +1,7 @@
 <div>
   <h3 class="h3-title uk-text-center uk-center"><?php the_title(); ?></h3>
   <div class="uk-card uk-card-default articles-cards-events">
-    <a href="<?php echo get_permalink() ?>">
+    <a href="<?php get_permalink() ?>">
       <img class="articles-cards-image-events" src="<?php the_post_thumbnail_url(); ?>" alt="">
     </a>
   </div>
