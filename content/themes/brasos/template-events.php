a8a5b3e1dcd49d527416f8d4382780772810f785
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-10 04:55:09 +0100

    finally I did it! CPT events ready

--- content/themes/brasos/template-events.php
+++ content/themes/brasos/template-events.php
@@ -3,18 +3,17 @@
 Template Name: Eventos
 */
 ?>
-<?php get_header(); ?>
 
+<?php get_header(); ?>
 
 <div class="articles-cards">
   <h2 class="h2-title"><?php the_title(); ?></h2>
   <div class="uk-child-width-1-3@m" uk-grid>
 
     <?php
-
     $args = [
       'post_type' => 'evento',
-      'category__in' => 1, // category 1 = parent "evento"
+      'category__in' => 1,
       'posts_per_page' => -1,
       'order' => 'DESC'
     ];
@@ -22,8 +21,10 @@ Template Name: Eventos
     $wp_query = new WP_Query($args);
 
     if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
+        
         get_template_part('template-parts/post/event', 'excerpt');
-      endwhile;
+
+    endwhile;
     endif;
 
     wp_reset_postdata();
@@ -31,6 +32,5 @@ Template Name: Eventos
 
   </div>
 </div>
-
 
 <?php get_footer(); ?>
