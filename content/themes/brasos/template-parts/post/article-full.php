d8555747967a73fbbdeee5a9434491acdc82bbae
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-13 20:45:42 +0100

    layout adjusted

--- content/themes/brasos/template-parts/post/article-full.php
+++ content/themes/brasos/template-parts/post/article-full.php
@@ -1,14 +1,8 @@
-<div class="blog__cover">
-  <div class="uk-child-width-1-1" uk-grid>
-    <div>
-      <div class="uk-card uk-card-default" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
-        <div class="uk-card-body">
-          <h3 class="uk-card-title"><?php the_title(); ?></h3>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="events-cover uk-child-width-1-1">
+  <img class="events-cover__img" src="<?php the_post_thumbnail_url(); ?>">
+  <h1 class="h1-title"><?php the_title(); ?></h1>
 </div>
+
 <div class="blog__content">
   <?php the_content(); ?>
 </div>
