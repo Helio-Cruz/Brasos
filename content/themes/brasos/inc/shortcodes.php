7c6a9c55fd519c4c54a1be538eedaa64612cf430
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-13 17:12:28 +0100

    layout melhorias

--- content/themes/brasos/inc/shortcodes.php
+++ content/themes/brasos/inc/shortcodes.php
@@ -1,7 +1,7 @@
 <?php 
 
 function button_certificados(){
-  return '<div class="uk-flex uk-height-small uk-margin uk-text-center"><a class="uk-margin-auto uk-margin-auto-vertical normal-button special-button certificates-button" href="' . home_url('/certificados/') . '">Certificados</a></div>';
+  return '<div class="uk-flex uk-height-small uk-margin uk-text-center"><a class="uk-margin-auto uk-margin-auto-vertical normal-button special-button margin-button" href="' . home_url('/certificados/') . '">Certificados</a></div>';
 }
 add_shortcode('certificados', 'button_certificados');
 
