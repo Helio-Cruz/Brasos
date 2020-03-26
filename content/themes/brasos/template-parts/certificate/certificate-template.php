9f52b5e5d60f9d816e22cda610608c2ea7b18b89
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-03-26 12:03:37 +0100

    certificates showing only for user logged_in and selected in acf user field

--- content/themes/brasos/template-parts/certificate/certificate-template.php
+++ content/themes/brasos/template-parts/certificate/certificate-template.php
@@ -12,50 +12,52 @@
  </div> -->
 
 
-
     <div class="diplom__show">
 
       <?php
-      get_current_user_id();
       $args = [
         'post_type' => 'certificados',
         'posts_per_page' => -1,
-        'order' => 'ASC'
+        'order' => 'ASC',
       ];
-      $wp_query = new WP_Query($args);
-      if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
-      ?>
+      $wp_query = new WP_Query($args); ?>
+
+      <?php if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+
           <?php
-          $user = get_field('nome_do_palestrante');
-          if ($user) : ?>
-            <h3 class="h3-title"><?php echo $user['display_name']; ?></h3>
-          <?php endif; ?>
+          $current_user = get_current_user_id();
+          $selected_user = get_field('nome_do_palestrante');
+          if ($current_user == $selected_user['ID']) : ?>
 
-          <?php if (have_rows('certificados')) : while (have_rows('certificados')) : the_row(); ?>
+            <?php if ($selected_user) : ?>
+            <h3 class="h3-title"><?php echo $selected_user['display_name']; ?></h3>
+            <?php endif; ?>
 
-              <div uk-flex uk-flex-center uk-grid>
-                <div class="uk-card uk-card-default uk-card-hover">
+            <div uk-flex uk-flex-center uk-grid>
+              <?php if (have_rows('certificados')) : while (have_rows('certificados')) : the_row(); ?>
                   <?php $file = get_sub_field('certificado'); ?>
-                  <a href="<?php echo $file['url']; ?>" target="_blank">
-                    <div class="uk-card-header">
-                      <img class="uk-border-circle" src="<?php echo get_template_directory_uri() . '/public/images/certificate_icon.png' ?>">
-                    </div>
-                    <div class="uk-card-body">
-                      <h3 class="h3-title"><?php the_sub_field('titulo_do_certificado'); ?></h3>
-                      <p>
-                        <a class="download_image" href="<?php echo $file['url']; ?>" download>Clique para fazer download</a>
-                      </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
+                  <?php $title = get_sub_field('titulo_do_certificado'); ?>
+                  <div class="uk-card uk-card-default uk-card-hover">
+                    <a href="<?php echo $file['url']; ?>" target="_blank">
+                      <div class="uk-card-header">
+                        <img class="uk-border-circle" src="<?php echo get_template_directory_uri() . '/public/images/certificate_icon.png' ?>">
+                      </div>
+                      <div class="uk-card-body">
+                        <h3 class="h3-title"><?php echo $title; ?></h3>
+                        <p>
+                          <a class="download_image" href="<?php echo $file['url']; ?>" download>Clique para fazer download</a>
+                        </p>
+                      </div>
+                    </a>
+                  </div>
+              <?php endwhile;
+              endif; ?>
+            </div>
 
-          <?php endwhile;
-          endif; ?>
+          <?php endif; ?>
 
       <?php endwhile;
       endif; ?>
-
     </div>
   </div>
 </div>
