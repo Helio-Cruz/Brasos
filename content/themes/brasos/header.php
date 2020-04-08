270a73db11a2468542d6404ea6939fd5577b5f07
helio <helio-cruz@hotmail.com>
2020-04-08 08:46:59 +0200

    adding footer carousel theme mod

--- content/themes/brasos/header.php
+++ content/themes/brasos/header.php
@@ -3,10 +3,9 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Brazilian Society for Osteoarthritis, Osteoporosis and Sarcopenia">
   <link rel="icon" href=" <?php echo get_template_directory_uri() . '/public/images/favicon.ico' ?>">
   <title><?php bloginfo('name'); ?></title>
-  <!-- <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="scss/main.scss" type="text/scss" /> -->
 
   <?php wp_head(); ?>
 </head>
