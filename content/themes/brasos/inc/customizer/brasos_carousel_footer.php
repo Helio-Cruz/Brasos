1e9148fc7ce347d49973ede2b4e369ce933e6580
helio <helio-cruz@hotmail.com>
2020-04-08 08:46:59 +0200

    adding footer carousel theme mod

---
+++ content/themes/brasos/inc/customizer/brasos_carousel_footer.php
@@ -0,0 +1,466 @@
+<?php
+
+
+function brasos_carousel_footer($wp_customize) {
+
+
+    $wp_customize->add_setting('brasos_footer_image_1', []);
+    $wp_customize->add_setting('brasos_footer_image_2', []);
+    $wp_customize->add_setting('brasos_footer_image_3', []);
+    $wp_customize->add_setting('brasos_footer_image_4', []);
+    $wp_customize->add_setting('brasos_footer_image_5', []);
+    $wp_customize->add_setting('brasos_footer_image_6', []);
+    $wp_customize->add_setting('brasos_footer_image_7', []);
+    $wp_customize->add_setting('brasos_footer_image_8', []);
+    $wp_customize->add_setting('brasos_footer_image_9', []);
+    $wp_customize->add_setting('brasos_footer_image_10', []);
+    $wp_customize->add_setting('brasos_footer_image_11', []);
+    $wp_customize->add_setting('brasos_footer_image_12', []);
+    $wp_customize->add_setting('brasos_footer_image_13', []);
+    $wp_customize->add_setting('brasos_footer_image_14', []);
+    $wp_customize->add_setting('brasos_footer_image_15', []);
+    $wp_customize->add_setting('brasos_footer_image_16', []);
+    $wp_customize->add_setting('brasos_footer_image_17', []);
+    $wp_customize->add_setting('brasos_footer_image_18', []);
+    $wp_customize->add_setting('brasos_footer_image_19', []);
+    $wp_customize->add_setting('brasos_footer_image_20', []);
+    $wp_customize->add_setting('brasos_footer_image_21', []);
+    $wp_customize->add_setting('brasos_footer_image_22', []);
+    $wp_customize->add_setting('brasos_footer_image_23', []);
+    $wp_customize->add_setting('brasos_footer_image_24', []);
+    $wp_customize->add_setting('brasos_footer_image_25', []);
+    $wp_customize->add_setting('brasos_footer_image_26', []);
+    $wp_customize->add_setting('brasos_footer_image_27', []);
+    $wp_customize->add_setting('brasos_footer_image_28', []);
+    $wp_customize->add_setting('brasos_footer_image_29', []);
+    $wp_customize->add_setting('brasos_footer_image_30', []);
+    $wp_customize->add_setting('brasos_footer_image_31', []);
+    $wp_customize->add_setting('brasos_footer_image_32', []);
+    $wp_customize->add_setting('brasos_footer_image_33', []);
+    $wp_customize->add_setting('brasos_footer_image_34', []);
+    $wp_customize->add_setting('brasos_footer_image_35', []);
+    $wp_customize->add_setting('brasos_footer_image_36', []);
+    $wp_customize->add_setting('brasos_footer_image_37', []);
+    $wp_customize->add_setting('brasos_footer_image_38', []);
+    $wp_customize->add_setting('brasos_footer_image_39', []);
+    $wp_customize->add_setting('brasos_footer_image_40', []);
+
+
+
+
+ 
+
+
+    $footer_image =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos',
+        [
+            'label'    => 'Mude a Imagem 1',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_1',
+        ]
+        
+    );
+
+    $footer_image_2 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-2',
+        [
+            'label'    => 'Mude a Imagem 2',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_2',
+        ]
+    );
+   
+    $footer_image_3 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-3',
+        [
+            'label'    => 'Mude a Imagem 3',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_3',
+        ]
+    );
+
+    $footer_image_4 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-4',
+        [
+            'label'    => 'Mude a Imagem 4',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_4',
+        ]
+    );
+    
+    $footer_image_5 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-5',
+        [
+            'label'    => 'Mude a Imagem 5',
+            'section'  =>  'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_5',
+        ]
+    );
+
+    $footer_image_6 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-6',
+        [
+            'label'    => 'Mude a Imagem 6',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_6',
+        ]
+    );
+
+    $footer_image_7 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-7',
+        [
+            'label'    => 'Mude a Imagem 7',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_7',
+        ]
+    );
+    $footer_image_8 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-8',
+        [
+            'label'    => 'Mude a Imagem 8',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_8',
+        ]
+    );
+    $footer_image_9 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-9',
+        [
+            'label'    => 'Mude a Imagem 9',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_9',
+        ]
+    );
+    $footer_image_10 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-10',
+        [
+            'label'    => 'Mude a Imagem 10',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_10',
+        ]
+    );
+    $footer_image_11 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-11',
+        [
+            'label'    => 'Mude a Imagem 11',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_11',
+        ]
+    );
+    $footer_image_12 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-12',
+        [
+            'label'    => 'Mude a Imagem 12',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_12',
+        ]
+    );
+    $footer_image_13 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-13',
+        [
+            'label'    => 'Mude a Imagem 13',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_13',
+        ]
+    );
+    $footer_image_14 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-14',
+        [
+            'label'    => 'Mude a Imagem 14',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_14',
+        ]
+    );
+    $footer_image_15 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-15',
+        [
+            'label'    => 'Mude a Imagem 15',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_15',
+        ]
+    );
+    $footer_image_16 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-16',
+        [
+            'label'    => 'Mude a Imagem 16',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_16',
+        ]
+    );
+    $footer_image_17 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-17',
+        [
+            'label'    => 'Mude a Imagem 17',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_12',
+        ]
+    );
+    $footer_image_18 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-18',
+        [
+            'label'    => 'Mude a Imagem 18',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_18',
+        ]
+    );
+    $footer_image_19 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-19',
+        [
+            'label'    => 'Mude a Imagem 19',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_19',
+        ]
+    );
+    $footer_image_20 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-20',
+        [
+            'label'    => 'Mude a Imagem 20',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_20',
+        ]
+    );
+    $footer_image_21 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-21',
+        [
+            'label'    => 'Mude a Imagem 21',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_21',
+        ]
+    );
+    $footer_image_22 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-22',
+        [
+            'label'    => 'Mude a Imagem 22',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_22',
+        ]
+    );
+    $footer_image_23 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-23',
+        [
+            'label'    => 'Mude a Imagem 23',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_23',
+        ]
+    );
+   
+    $footer_image_24 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-24',
+        [
+            'label'    => 'Mude a Imagem 24',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_24',
+        ]
+    );
+    $footer_image_25 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-25',
+        [
+            'label'    => 'Mude a Imagem 25',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_25',
+        ]
+    );
+    $footer_image_26 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-26',
+        [
+            'label'    => 'Mude a Imagem 26',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_26',
+        ]
+    );
+    $footer_image_27 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-27',
+        [
+            'label'    => 'Mude a Imagem 27',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_27',
+        ]
+    );
+    $footer_image_28 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-28',
+        [
+            'label'    => 'Mude a Imagem 28',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_28',
+        ]
+    );
+    $footer_image_29 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-29',
+        [
+            'label'    => 'Mude a Imagem 29',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_29',
+        ]
+    );
+    $footer_image_30 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-30',
+        [
+            'label'    => 'Mude a Imagem 30',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_30',
+        ]
+    );
+    $footer_image_31 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-31',
+        [
+            'label'    => 'Mude a Imagem 31',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_31',
+        ]
+    );
+    $footer_image_32 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-32',
+        [
+            'label'    => 'Mude a Imagem 32',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_32',
+        ]
+    );
+    $footer_image_33 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-33',
+        [
+            'label'    => 'Mude a Imagem 33',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_33',
+        ]
+    );
+    $footer_image_34 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-34',
+        [
+            'label'    => 'Mude a Imagem 34',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_34',
+        ]
+    );
+    $footer_image_35 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-35',
+        [
+            'label'    => 'Mude a Imagem 35',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_35',
+        ]
+    );
+    $footer_image_36 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-36',
+        [
+            'label'    => 'Mude a Imagem 36',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_36',
+        ]
+    );
+    $footer_image_37 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-37',
+        [
+            'label'    => 'Mude a Imagem 37',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_37',
+        ]
+    );
+    $footer_image_38 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-38',
+        [
+            'label'    => 'Mude a Imagem 38',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_38',
+        ]
+    );
+    $footer_image_39 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-39',
+        [
+            'label'    => 'Mude a Imagem 39',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_39',
+        ]
+    );
+    $footer_image_40 =  new WP_Customize_Image_Control(
+        $wp_customize,
+        'footer-brasos-40',
+        [
+            'label'    => 'Mude a Imagem 40',
+            'section'  => 'brasos_carousel_footer',
+            'settings' => 'brasos_footer_image_40',
+        ]
+    );
+    $wp_customize->add_control($footer_image);
+    $wp_customize->add_control($footer_imag_1);
+    $wp_customize->add_control($footer_image_2);
+    $wp_customize->add_control($footer_image_3);
+    $wp_customize->add_control($footer_image_4);
+    $wp_customize->add_control($footer_image_5);
+    $wp_customize->add_control($footer_image_6);
+    $wp_customize->add_control($footer_image_7);
+    $wp_customize->add_control($footer_image_8);
+    $wp_customize->add_control($footer_image_9);
+    $wp_customize->add_control($footer_image_10);
+    $wp_customize->add_control($footer_image_11);
+    $wp_customize->add_control($footer_image_12);
+    $wp_customize->add_control($footer_image_13);
+    $wp_customize->add_control($footer_image_14);
+    $wp_customize->add_control($footer_image_15);
+    $wp_customize->add_control($footer_image_16);
+    $wp_customize->add_control($footer_image_17);
+    $wp_customize->add_control($footer_image_18);
+    $wp_customize->add_control($footer_image_19);
+    $wp_customize->add_control($footer_image_20);
+    $wp_customize->add_control($footer_image_21);
+    $wp_customize->add_control($footer_image_22);
+    $wp_customize->add_control($footer_image_23);
+    $wp_customize->add_control($footer_image_24);
+    $wp_customize->add_control($footer_image_25);
+    $wp_customize->add_control($footer_image_26);
+    $wp_customize->add_control($footer_image_27);
+    $wp_customize->add_control($footer_image_28);
+    $wp_customize->add_control($footer_image_29);
+    $wp_customize->add_control($footer_image_30);
+    $wp_customize->add_control($footer_image_31);
+    $wp_customize->add_control($footer_image_32);
+    $wp_customize->add_control($footer_image_33);
+    $wp_customize->add_control($footer_image_34);
+    $wp_customize->add_control($footer_image_35);
+    $wp_customize->add_control($footer_image_36);
+    $wp_customize->add_control($footer_image_37);
+    $wp_customize->add_control($footer_image_38);
+    $wp_customize->add_control($footer_image_39);
+    $wp_customize->add_control($footer_image_40);
+
+ 
+
+}
---
+++ content/themes/brasos/template-parts/home/footer-images.php
@@ -0,0 +1,126 @@
+<div uk-slider="autoplay: true; autoplay-interval: 3000; infinite:true;" class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1"   >
+        <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-6@m autoplay">
+            <li>
+             <img src="<?= get_theme_mod('brasos_footer_image_1'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_2'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_3'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_4'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_5'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_6'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_7'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_8'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_9'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_10'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_11'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_12'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_13'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_14'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_15'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_16'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_17'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_18'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_19'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_20'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_21'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_22'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_23'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_24'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_25'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_26'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_27'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_28'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_29'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_30'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_31'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_32'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_33'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_34'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_35'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_36'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_37'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_38'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_39'); ?>" alt="">
+            </li>
+            <li>
+            <img src="<?= get_theme_mod('brasos_footer_image_40'); ?>" alt="">
+            </li>
+        </ul>
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+    </div>
