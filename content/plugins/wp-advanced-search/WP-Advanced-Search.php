622bcf5c3f453391e5c0ed374346a9f5dbd449df
helio <helio-cruz@hotmail.com>
2020-04-08 15:27:29 +0200

    brasos wp-5.4 version'

--- content/plugins/wp-advanced-search/WP-Advanced-Search.php
+++ content/plugins/wp-advanced-search/WP-Advanced-Search.php
@@ -4,7 +4,7 @@ Plugin Name: WP-Advanced-Search
 Plugin URI: http://blog.internet-formation.fr/2013/10/wp-advanced-search/
 Description: Moteur de recherche avancé pour WordPress à la place du moteur initial (mise en surbrillance, trois types de recherche, styles, paginations, algorithme de pertinence optionnel...). (<em>Plugin adds a advanced search engine for WordPress with a lot of options (three type of search, bloded request, three method for pagination, relevancy algorithm...</em>).
 Author: Mathieu Chartier
-Version: 3.3.5
+Version: 3.3.6
 Author URI: http://blog.internet-formation.fr
 Text Domain: wp-advanced-search
 Domain Path: /lang
@@ -18,7 +18,7 @@ $tableName = 'advsh'; // Nom de la table
 $table_WP_Advanced_Search = $wpdb->prefix.$tableName;
 
 // Version du plugin
-$WP_Advanced_Search_Version = "3.3.5";
+$WP_Advanced_Search_Version = "3.3.6";
 
 function WP_Advanced_Search_Lang() {
 	// load_plugin_textdomain('wp-advanced-search', false, dirname(plugin_basename( __FILE__ )).'/lang/');
