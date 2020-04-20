c2b63712dcdaaaf6cd58aa89f704cbc619c873b8
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-04-20 06:10:18 +0200

    layout congressos

--- content/themes/brasos/template-congress.php
+++ content/themes/brasos/template-congress.php
@@ -7,7 +7,7 @@ Template Name: Congressos
 <?php get_header(); ?>
 
 <div uk-scrollspy="cls: uk-animation-fade; target: .box; delay: 500; repeat: false">
-<div class="congress__content">
+  <div class="congress__content">
     <h2 class="h2-title">Congressos</h2>
     <div uk-flex uk-flex-center uk-grid>
       <div class="uk-card uk-card-default box">
@@ -15,56 +15,45 @@ Template Name: Congressos
           <h3 class="uk-card-title h3-title"><a href="http://brasos.med.br/" target="_blank">Brasit 2020 <br> Brasos - Brazilian Symposium on Injection Treatment</a></h3>
         </div>
         <div class="uk-card-body">
-          <p>Local: Centro de  Convenções do Barra Brisa Hotel 
-            <br>
-            <br>
-             Rio de Janeiro - Brasil
-            <br><br>
-            Data: 24 - 27 de Setembro de 2020
-          </p>
+          <h4>Local</h4>
+          <p>Centro de Convenções do Barra Brisa Hotel<br>Rio de Janeiro - Brasil</p>
+          <h4>Data</h4>
+          <p>24 - 27 de Setembro de 2020</p>
         </div>
       </div>
       <div class="uk-card uk-card-default box">
-
         <div class="uk-card-header">
           <h3 class="uk-card-title h3-title"><a href="http://brasos.med.br/" target="_blank">2º SimGeos <br>Simpósio do Grupo de Estudos Em Osteoartrite, Osteoporose e Sarcopenia HCFMUSP - Santa Casa SP </a></h3>
         </div>
         <div class="uk-card-body">
-          <p>Local:  Faculdade de Ciências Médicas da Santa Casa Sao Paulo - Brasil
-            <br><br>
-            Data: 24 - 27 de Setembro de 2020
-            <br><br>
-          </p>
+          <h4>Local</h4>
+          <p>Faculdade de Ciências Médicas da Santa Casa<br>Sao Paulo - Brasil</p>
+          <h4>Data</h4>
+          <p>24 - 27 de Setembro de 2020</p>
         </div>
       </div>
       <div class="uk-card uk-card-default box">
-
         <div class="uk-card-header">
           <h3 class="uk-card-title h3-title"><a href="https://www.bradoo.com.br/" target="_blank">9º Bradoo Congresso Brasileiro de Densitometria, Osteoporose e Osteometabolismo</a></h3>
         </div>
-
         <div class="uk-card-body">
-          <p>Local: Centro de Convenções do Windsor Oceânico Hotel - Rio de Janeiro - Brasil
-            <br><br>
-            Data: 21 - 24 de Outubro de 2020
-            <br><br>
-          </p>
+          <h4>Local</h4>
+          <p>Centro de Convenções do Windsor Oceânico Hotel<br>Rio de Janeiro - Brasil</p>
+          <h4>Data</h4>
+          <p>21 - 24 de Outubro de 2020</p>
         </div>
-      </div>  
+      </div>
       <div class="uk-card uk-card-default box">
-
-<div class="uk-card-header">
-  <h3 class="uk-card-title h3-title"><a href="https://www.bradoo.com.br/" target="_blank">XXVII Congresso Brasileiro de Medicina Fisica e Reabilitaçao</a></h3>
-</div>
-
-<div class="uk-card-body">
-  <p>Local:  Hotel Bourbon Cataratas - Foz do Iguaçu, Paraná - Brasil
-    <br><br>
-    Data: 6 - 24 de Abril 2021
-    <br><br>
-  </p>
-</div>
-</div>  
+        <div class="uk-card-header">
+          <h3 class="uk-card-title h3-title"><a href="https://www.bradoo.com.br/" target="_blank">XXVII Congresso Brasileiro de Medicina Fisica e Reabilitaçao</a></h3>
+        </div>
+        <div class="uk-card-body">
+          <h4>Local</h4>
+          <p>Hotel Bourbon Cataratas<br>Foz do Iguaçu, Paraná - Brasil</p>
+          <h4>Data</h4>
+          <p>Data: 6 - 24 de Abril 2021</p>
+        </div>
+      </div>
     </div>
   </div>
 </div>
