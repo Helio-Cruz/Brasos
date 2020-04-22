ccdef35f1b77ec0a7340016fd1f052519877314d
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-04-22 22:22:25 +0200

    navbar loading faster

--- content/themes/brasos/header.php
+++ content/themes/brasos/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content=" Brasos Brazilian Society for Osteoarthritis, Osteoporosis and Sarcopenia">
   <link rel="icon" href=" <?php echo get_template_directory_uri() . '/public/images/favicon.ico' ?>">
-
-
   <?php wp_head(); ?>
 </head>
+
 <body>
-<?php  get_template_part('template-parts/header/header', 'navbar'); ?>
-<?php  get_template_part('template-parts/header/header', 'navbarmobile'); ?>
-<?php  get_template_part('template-parts/header/header', 'sidebar'); ?>
+  <?php get_template_part('template-parts/header/header', 'navbar'); ?>
+  <?php get_template_part('template-parts/header/header', 'navbarmobile'); ?>
+  <?php get_template_part('template-parts/header/header', 'sidebar'); ?>
