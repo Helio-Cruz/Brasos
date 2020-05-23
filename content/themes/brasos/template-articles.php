d0472cd98c5e30a182b5393ad25d4f1a732c8d83
helio <helio-cruz@hotmail.com>
2020-05-23 16:16:47 +0200

    ...

--- content/themes/brasos/template-articles.php
+++ content/themes/brasos/template-articles.php
@@ -10,7 +10,8 @@ Template Name: Artigos
 
   <div class="uk-child-width-1-1" uk-grid>
     <div uk-scrollspy="cls: uk-animation-slide-bottom; target: .box; delay: 300; repeat: false">
-      <?php
+     <!--
+      <//?php
       $recent_posts = wp_get_recent_posts(array(
         'orderby' => 'post_date',
         'order' => 'DESC',
@@ -21,20 +22,21 @@ Template Name: Artigos
       ));
       foreach ($recent_posts as $post) :
       ?>
-
+ 
         <div>
-          <div class="uk-card uk-card-default box" style="background-image: url('<?php echo get_the_post_thumbnail_url($post['ID']); ?>');">
-            <a href="<?php echo get_permalink($post['ID']) ?>">
+          <div class="uk-card uk-card-default box" style="background-image: url('<//?php echo get_the_post_thumbnail_url($post['ID']); ?>');">
+            <a href="<//?php echo get_permalink($post['ID']) ?>">
               <div class="uk-card-body">
-                <h3 class="uk-card-title h3-title"><?php echo $post['post_title'] ?></h3>
-                <!-- <p><//?php //echo substr(get_the_excerpt(), 0,65).'...'; ?></p> -->
-                <p><?php echo $excerpt = wp_trim_excerpt($post['post_excerpt']);  ?></p>
+                <h3 class="uk-card-title h3-title"><//?php echo $post['post_title'] ?></h3>
+                <!-- <p><//?php //echo substr(get_the_excerpt(), 0,65).'...'; ?></p> 
+                <p><//?php echo $excerpt = wp_trim_excerpt($post['post_excerpt']);  ?></p>-
               </div>
             </a>
           </div>
         </div>
-      <?php endforeach;
-      wp_reset_query(); ?>
+         
+      <//?php endforeach;
+      wp_reset_query(); ?>-->
     </div>
   </div>
   <div class="uk-child-width-1-3@m" uk-grid>
@@ -44,7 +46,7 @@ Template Name: Artigos
     $args = [
       'category_name' => 'Artigo',
       'order' => 'DESC',
-      'offset' => 1
+      //'offset' => 1
     ];
     $wp_query = new WP_Query($args);
     if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
