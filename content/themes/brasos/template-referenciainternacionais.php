2b5974378b528a0e95695b44ecdb424a8fa33c7c
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-05-24 11:47:46 +0200

    templates referencias inter-nacionais

--- content/themes/brasos/template-referenciainternacionais.php
+++ content/themes/brasos/template-referenciainternacionais.php
@@ -3,4 +3,25 @@
 Template Name: Refêrencias Internacionais
 */
 ?>
- 
+<?php get_header(); ?>
+<div class="ref-internacionais">
+  <h2>Referências Internacionais</h2>
+  <div class=" uk-align-center uk-flex uk-flex-center" uk-grid>
+    <div class="uk-flex uk-flex-middle uk-card uk-card-default uk-card-body ref-nacionais__card">
+      <img data-src="<?php echo get_template_directory_uri() . '/public/images/referencias/internacionais/A8HR6Y.jpg' ?>" alt="" uk-img>
+    </div>
+    <div class="uk-flex uk-flex-middle uk-card uk-card-default uk-card-body ref-nacionais__card">
+      <img data-src="<?php echo get_template_directory_uri() . '/public/images/referencias/internacionais/OBCrHw.jpg' ?>" alt="" uk-img>
+    </div>
+  </div>
+
+  <div class="uk-align-center uk-flex uk-flex-center" uk-grid>
+    <div class="uk-flex uk-flex-middle uk-card uk-card-default uk-card-body ref-nacionais__card">
+      <img data-src="<?php echo get_template_directory_uri() . '/public/images/referencias/internacionais/uRv0Mh.jpg' ?>" alt="" uk-img>
+    </div>
+    <div class="uk-flex uk-flex-middle uk-card uk-card-default uk-card-body ref-nacionais__card">
+      <img data-src="<?php echo get_template_directory_uri() . '/public/images/referencias/internacionais/zJRGns.png' ?>" alt="" uk-img>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
