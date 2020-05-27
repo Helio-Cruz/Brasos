2182ef2418a8e09c792ebdf7bd603c3830d26588
helio <helio-cruz@hotmail.com>
2020-05-27 22:53:31 +0200

    ..

--- content/themes/brasos/index.php
+++ content/themes/brasos/index.php
@@ -1,11 +1,1 @@
-<?php get_header(); ?>
-
-<?php  get_template_part('template-parts/home/home', 'carousel'); ?>  
-<?php  get_template_part('template-parts/home/home', 'carousel-2'); ?>  
-<?php  get_template_part('template-parts/home/home', 'blog'); ?>  
-<?php  get_template_part('template-parts/home/home', 'brasos'); ?>  
-<?php  get_template_part('template-parts/home/footer', 'images'); ?>  
  
-
-
-<?php get_footer(); ?>
