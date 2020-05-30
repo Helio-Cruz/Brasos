026099df0d97fd44bfea6d9222cea8a6e6e04365
helio <helio-cruz@hotmail.com>
2020-05-30 22:30:36 +0200

    last version

--- content/themes/brasos/template-producaocientifica.php
+++ content/themes/brasos/template-producaocientifica.php
@@ -1,20 +1,17 @@
 <?php
 /*
-Template Name: Eventos
+Template Name: Producao Cientifica
 */
 ?>
-
 <?php get_header(); ?>
 
 <div class="articles-cards eventos-cards-bloc">
-<h2 class="h2-title"><?php the_title(); ?></h2>
+<h2 class="h2-title">Produção Científica Brasos</h2>
   <div class="eventos-cards" uk-grid uk-scrollspy="cls: uk-animation-slide-bottom; target: .box; delay: 300; repeat: false">
 
     <?php
     $args = [
-      'post_type' => 'evento',
-      'category__in' => 1, // category 1 = parent "evento"
-      'posts_per_page' => -1,
+      'category_name' => 'Cientifica',
       'order' => 'DESC'
     ];
 
@@ -22,12 +19,11 @@ Template Name: Eventos
 
     if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
         
-        get_template_part('template-parts/post/event', 'excerpt');
+        get_template_part('template-parts/post/news', 'excerpt');
 
     endwhile;
     endif;
-
-    wp_reset_postdata();
+    wp_reset_query();
     ?>
 
   </div>
