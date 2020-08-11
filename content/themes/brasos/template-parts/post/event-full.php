0db3bc9923ecdef6741de481aede6bb220ce9ea9
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-08-11 12:01:40 +0200

    eventos : inscriçao

--- content/themes/brasos/template-parts/post/event-full.php
+++ content/themes/brasos/template-parts/post/event-full.php
@@ -148,25 +148,33 @@
     <?php endif; ?>
 
     <!-- FORMULARIO DE INSCRIÇAO  -->
-    <?php if (get_field('formulario_de_inscricao')) : ?>
-      <div class="blog__content inscriptions__content">
-        <h2 class="h2-title">FORMULARIO DE INSCRIÇÃO</h2>
-        <?php the_field('formulario_de_inscricao'); ?>
-        <div class="uk-flex uk-height-small uk-margin uk-text-center">
-          <a class="uk-margin-auto uk-margin-auto-vertical normal-button special-button margin-button" href="#modal-form" uk-toggle><?php the_title(); ?></a>
-        </div>
-      </div>
-      <!-- FORM BUTTON -->
-      <div id="modal-form" class="uk-flex-top" uk-modal>
-        <div class="uk-modal-dialog uk-margin-auto-vertical contact-form contact-form-inscription">
-          <h2 class="h2-title">Inscrição</h2>
-          <button class="uk-modal-close-default" type="button" uk-close></button>
-          <div class="contact-form-block">
-            <?php echo do_shortcode('[contact-form-7 id="301" title="Inscrição"]'); ?>
+    <?php global $post;
+    if ($post->ID == 1419) : ?>
+      <?php if (get_field('formulario_de_inscricao')) : ?>
+        <div class="blog__content inscriptions__content">
+          <h2 class="h2-title">FORMULARIO DE INSCRIÇÃO</h2>
+          <?php the_field('formulario_de_inscricao'); ?>
+          <div class="uk-flex uk-height-small uk-margin uk-text-center">
+            <a class="uk-margin-auto uk-margin-auto-vertical normal-button special-button margin-button" href="#modal-form" uk-toggle><?php the_title(); ?></a>
           </div>
         </div>
-      </div>
-    <?php endif; ?>
+        <!-- FORM BUTTON -->
+        <div id="modal-form" class="uk-flex-top" uk-modal>
+          <div class="uk-modal-dialog uk-margin-auto-vertical contact-form contact-form-inscription">
+            <h2 class="h2-title">Inscrição</h2>
+            <button class="uk-modal-close-default" type="button" uk-close></button>
+            <div class="contact-form-block">
+              <?php echo do_shortcode('[contact-form-7 id="301" title="Inscrição"]'); ?>
+            </div>
+          </div>
+        </div>
+      <?php endif; ?>
+    <?php else : ?>
+      <?php if (get_field('formulario_de_inscricao')) : ?>
+        <div class="blog__content inscriptions__content">
+          <?php the_field('formulario_de_inscricao'); ?>
+        <?php endif; ?>
+      <?php endif; ?>
   </li>
 
 
