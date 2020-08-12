a9a16b1439f8176cea351cd8a436f64a7e4176f6
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-08-12 23:36:22 +0200

    pop up link + events layout

--- content/themes/brasos/home.php
+++ content/themes/brasos/home.php
@@ -3,14 +3,15 @@ $popup = get_theme_mod('brasos_popup_image');
 $default = null;
 $img = !empty($popup) ? $popup : $default;
 ?>
-  <!-- POP UP -->
-  <!-- This is a button toggling the modal with the outside close button -->
-  <div id="modal-media-image" class="uk-flex-top" uk-modal>
-    <div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical">
-      <button class="uk-modal-close-outside" type="button" uk-close></button>
-      <img class="modal-image" src="<?= $img; ?>" alt="">
-    </div>
+<!-- POP UP -->
+<!-- This is a button toggling the modal with the outside close button -->
+<div id="modal-media-image" class="uk-flex-top" uk-modal>
+  <div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical">
+    <button class="uk-modal-close-outside" type="button" uk-close></button>
+    <img class="modal-image" src="<?= $img; ?>" alt="">
+    <?php echo do_shortcode('[link_popup link="https://regency.iweventos.com.br/site/brasit2020/inscreva-se" text="Mais informações e inscrição, Acesse"]'); ?>
   </div>
+</div>
 
 <?php get_template_part('template-parts/home/home', 'carousel'); ?>
 <?php get_template_part('template-parts/home/home', 'carousel-2'); ?>
--- content/themes/brasos/inc/shortcodes.php
+++ content/themes/brasos/inc/shortcodes.php
@@ -1,13 +1,18 @@
-<?php 
+<?php
 
-function button_certificados(){
+function button_certificados()
+{
   return '<div class="uk-flex uk-height-small uk-margin uk-text-center"><a class="uk-margin-auto uk-margin-auto-vertical normal-button special-button margin-button" href="' . home_url('/certificados/') . '">Certificados</a></div>';
 }
 add_shortcode('certificados', 'button_certificados');
 
-    
-    
-
-   
-    
+function link_popup($atts)
+{
+  $text = isset( $atts['text'] )? $atts['text'] : '';
+  $link = isset( $atts['link'] )? $atts['link'] : '';
   
+  $html = '<a href="' . $link . '" target="_blank" class="popup-link">' . $text . '</a>';
+
+  return $html;
+}
+add_shortcode('link_popup', 'link_popup');
