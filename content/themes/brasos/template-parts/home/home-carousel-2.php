497764816a72bf1a7df89f70e98fad1be8493506
helio <helio-cruz@hotmail.com>
2020-10-28 11:45:02 +0100

    implementng new version

--- content/themes/brasos/template-parts/home/home-carousel-2.php
+++ content/themes/brasos/template-parts/home/home-carousel-2.php
@@ -1,77 +1,107 @@
 <!-- CAROUSEL -->
 <div class="slick-carousel-small__block">
-  <div class="slick-carousel-small__title">Brasit 2020 - Temática Preliminar<?= get_theme_mod('brasos_call_to_content-0'); ?></div>
-  <div class="slick-carousel-small">
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-1'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-2'); ?>
+  <div class="slick-carousel-small__position">
+    <h2 class="slick-carousel-small__title"><?= get_theme_mod('brasos_call_to_content-0'); ?></h2>
+      <ul class="slick-carousel-small">
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-1'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-2'); ?>
 
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-3'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-4'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-5'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-6'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-7'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-8'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-9'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-10'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-11'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-12'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-13'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-        <?= get_theme_mod('brasos_call_to_content-14'); ?>
-      </p>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-3'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-4'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-5'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-6'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-7'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-8'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-9'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-10'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-11'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-12'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-13'); ?>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              <?= get_theme_mod('brasos_call_to_content-14'); ?>
+            </p>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
-</div>
