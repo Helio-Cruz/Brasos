de4a833ef33d74c14727b876ae426f11e70cd1bc
helio <helio-cruz@hotmail.com>
2021-01-16 11:48:56 +0100

    changing buttons layout

--- content/themes/brasos/single-evento.php
+++ content/themes/brasos/single-evento.php
@@ -3,33 +3,31 @@
 <!-- IMAGE COVER -->
 <div class="events-cover uk-child-width-1-1" uk-grid>
   <img class="events-cover__img" src="<?php the_post_thumbnail_url(); ?>" alt="" />
-  <!-- <h1 class="h1-title "><//?php the_title(); ?></h1>-->
 </div>
 
 <!-- NAVIGATION -->
-
 <div class="uk-subnav navbar-events">
-  <a class="normal-link normal-link-one" target="_blank" href="https://regency.iweventos.com.br/site/brasit2020/inscreva-se">INSCRIÇÕES 2nd Brasit</a>
+  <?php 
+      $link = get_field('inscreva-se');
+      if ($link) :
+    ?>
+    <a class="normal-link normal-link-one" target="_blank" href="<?php echo esc_url($link);  ?>">INSCREVA-SE</a>
+  <?php endif; ?>
 </div>
 <ul class="uk-subnav navbar-events" uk-switcher>
-  <!-- < ?php if (have_rows('informacoes_basicas') || have_rows('mensagem_do_presidente') || get_field('comissao_organizadora') || have_rows('sobre_o_evento') || get_field('turismo')) : ?> -->
-  <li><a href="#">INFORMAÇÕES GERAIS</a></li>
-  <!-- < ?php endif; ?> -->
-  <!-- < ?php if (have_rows('programacao')) : ?> -->
-  <li><a href="#">PROGRAMAÇÃO</a></li>
-  <!-- < ?php endif; ?> -->
-  <!-- < ?php if (get_field('palestrantes')) : ?> -->
-  <li><a href="#">PALESTRANTES CONVIDADOS</a></li>
-  <!-- < ?php endif; ?> -->
-  <!-- < ?php if (have_rows('valores') || have_rows('cursos') || get_field('formulario_de_inscricao')) : ?> -->
-  <!-- <li><a href="#">INSCRIÇÕES/PAGAMENTO</a></li> -->
-  <!-- <a class="last-link" target="_blank" href="https://regency.iweventos.com.br/site/brasit2020/inscreva-se">INSCRIÇÕES 2nd Brasit</a> -->
-  <!-- < ?php endif; ?> -->
+  <li>
+    <a href="#">INFORMAÇÕES GERAIS</a>
+  </li>
+  <li>
+    <a href="#">PROGRAMAÇÃO</a>
+  </li>
+  <li>
+    <a href="#">PALESTRANTES CONVIDADOS</a>
+  </li>
 </ul>
 <div class="uk-subnav navbar-events">
   <a class="normal-link" href="<?php echo site_url('/certificados/'); ?>">CERTIFICADOS</a>
 </div>
-
 
 <?php
 $current_page_id = get_the_ID();
