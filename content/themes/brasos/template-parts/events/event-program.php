cbc075125f2909833db9fab9cb4c9fa1713f5b54
Claire-Lavigne <lavigneclaire81@gmail.com>
2021-07-26 14:48:07 +0200

    layout adjusted

--- content/themes/brasos/template-parts/events/event-program.php
+++ content/themes/brasos/template-parts/events/event-program.php
@@ -19,40 +19,31 @@
               endif; ?>
 
               <?php if (have_rows('detalhe')) : while (have_rows('detalhe')) : the_row();
+                  $modulo = get_sub_field('modulo_');
                   $startHour = get_sub_field('horario_inicial');
                   $endHour = get_sub_field('horario_final');
+                  $subtitle = get_sub_field('subtitulo');
               ?>
                   <div class="full-content">
-                    <!-- TIMELINE -->
-                    <div class="left-content">
-                      <?php if ($startHour) : ?>
-                        <div class="start-hour">
-                          <?= $startHour ?>
-                        </div>
-                      <?php endif; ?>
-                      <?php if ($endHour) : ?>
-                        <div class="end-hour">
-                          <?= $endHour ?>
-                        </div>
-                      <?php endif; ?>
-                    </div>
+                    <?php if (!$modulo && $startHour) : ?>
+                      <div class="start-hour"> <?= $startHour ?> </div>
+                    <?php endif; ?>
                     <!-- CARDS -->
                     <?php if (have_rows('cards')) : ?>
-                      <div class="right-content">
-                        <?php if ( get_sub_field('subtitulo') ) : ?>
-                          <div class="subtitle">
-                            <?= get_sub_field('subtitulo'); ?>
-                          </div>
+                      <div class="right-content <?php if (!$modulo) : ?>right-content-border<?php endif; ?>">
+                        <?php if ($modulo && $subtitle) : ?> <h2 class="subtitle"><?= $subtitle ?></h2>
+                        <?php elseif (!$modulo && $subtitle) : ?> <h3 class="subtitle"><?= $subtitle ?></h3>
                         <?php endif; ?>
+                        <!-- CARDS -->
                         <?php while (have_rows('cards')) : the_row();
                           $img = get_sub_field('imagem_de_perfil');
                           $description = get_sub_field('descricao_da_pessoa');
                           $nationality = get_sub_field('nacionalidade');
                         ?>
                           <div class="card">
-                            <div class="card-image"><img src="<?php echo esc_url($img['url']); ?>" alt="<?php echo esc_attr($img['alt']); ?>" /></div>
+                            <div class="card-image"><img src="<?= esc_url($img['url']); ?>" alt="<?= esc_attr($img['alt']); ?>" /></div>
                             <div class="card-text"><?= $description ?></div>
-                            <div class="card-flag"><img src="<?= $nationality ?>" /></div>
+                            <?php if ($nationality) : ?><div class="card-flag"><img src="<?= $nationality ?>" /></div><?php endif; ?>
                           </div>
                           <?php if (have_rows('patrocinadores')) : ?>
                             <div class="patrocinadores">
@@ -64,6 +55,9 @@
                           <?php endif; ?>
                         <?php endwhile; ?>
                       </div>
+                    <?php endif; ?>
+                    <?php if (!$modulo && $endHour) : ?>
+                      <div class="end-hour"> <?= $endHour ?> </div>
                     <?php endif; ?>
                   </div>
 
